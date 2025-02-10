c71b0a52ce1ead8faa28f4f0bcf935b4d545469d
Deeplerg <deeplerg@gmail.com>
2025-02-10 09:01:54 +0500

    Add thymeleaf and index template

--- src/main/java/com/example/demo/controller/PostsViewController.java
+++ src/main/java/com/example/demo/controller/PostsViewController.java
@@ -1,17 +1,21 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class PostsViewController {
     @ResponseBody
-    @RequestMapping(path = "/", method = RequestMethod.GET)
-    public String list() {
-        return "Здесь будет главная страница";
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public ModelAndView messages() {
+        ModelAndView mav = new ModelAndView("list");
+        mav.addObject("appName", "Моё супер приложение");
+        return mav;
     }
 
     @ResponseBody
