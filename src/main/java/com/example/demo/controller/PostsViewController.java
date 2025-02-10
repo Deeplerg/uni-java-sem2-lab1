05559938f0bdaca2a0fc7b417569389da569aa7c
Deeplerg <deeplerg@gmail.com>
2025-02-10 09:18:11 +0500

    Bring back list without explicit ModelAndView

--- src/main/java/com/example/demo/controller/PostsViewController.java
+++ src/main/java/com/example/demo/controller/PostsViewController.java
@@ -3,6 +3,7 @@ package com.example.demo.controller;
 import com.example.demo.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -18,13 +19,11 @@ public class PostsViewController {
         this.postService = postService;
     }
 
-    @ResponseBody
-    @RequestMapping(value = "/", method = RequestMethod.GET)
-    public ModelAndView list() {
-        ModelAndView mav = new ModelAndView("list");
-        mav.addObject("appName", "Моё супер приложение");
-        mav.addObject("posts    ", postService.listAllPosts());
-        return mav;
+    @RequestMapping(path = "/", method = RequestMethod.GET)
+    public String list(Model model) {
+        model.addAttribute("appName", "Моё супер приложение");
+        model.addAttribute("posts", postService.listAllPosts());
+        return "list";
     }
 
     @ResponseBody
