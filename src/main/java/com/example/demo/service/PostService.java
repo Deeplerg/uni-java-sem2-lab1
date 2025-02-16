3962f285b48026aafc4d0c16408a99d6a0be4c8c
Deeplerg <deeplerg@gmail.com>
2025-02-16 23:21:34 +0500

    Add ability to like posts

--- src/main/java/com/example/demo/service/PostService.java
+++ src/main/java/com/example/demo/service/PostService.java
@@ -4,9 +4,7 @@ import com.example.demo.model.Post;
 import com.sun.source.tree.ArrayAccessTree;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 
 @Service
 public class PostService {
@@ -14,9 +12,9 @@ public class PostService {
 
     public PostService() {
         posts = new ArrayList<>();
-        posts.add(new Post("Random post 1", new Date(2024, 10, 15, 14, 00)));
-        posts.add(new Post("Random post 2", new Date(2024, 10, 15, 15, 17)));
-        posts.add(new Post("Random post 3", new Date(2024, 11, 12, 17, 35)));
+        posts.add(new Post(0L, "Random post 1", new Date(2024, 10, 15, 14, 00)));
+        posts.add(new Post(1L, "Random post 2", new Date(2024, 10, 15, 15, 17)));
+        posts.add(new Post(2L, "Random post 3", new Date(2024, 11, 12, 17, 35)));
     }
 
     public List<Post> listAllPosts() {
@@ -24,6 +22,9 @@ public class PostService {
     }
 
     public void create(String text) {
-        posts.add(new Post(text, new Date()));
+        Long maxId = posts.getLast().getId(); // won't work properly if we remove posts
+
+        var post = new Post(maxId + 1, text, new Date());
+        posts.add(post);
     }
 }
