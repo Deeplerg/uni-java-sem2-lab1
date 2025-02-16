6e9b8d8fcea3ce58d21a5bb44df114c8034b9442
Deeplerg <deeplerg@gmail.com>
2025-02-16 21:16:08 +0500

    Add Bootstrap and creation date

--- src/main/java/com/example/demo/model/Post.java
+++ src/main/java/com/example/demo/model/Post.java
@@ -1,16 +1,15 @@
 package com.example.demo.model;
 
+import java.util.Date;
+
 public class Post {
     private String text;
     private Integer likes;
+    private Date creationDate;
 
-    public Post(String text) {
+    public Post(String text, Date creationDate) {
         this.text = text;
-    }
-
-    public Post(String text, Integer likes) {
-        this.text = text;
-        this.likes = likes;
+        this.creationDate = creationDate;
     }
 
     public String getText() {
@@ -19,5 +18,11 @@ public class Post {
 
     public Integer getLikes() {
         return likes;
+    }
+
+    public void setLikes(Integer likes) { this.likes = likes; }
+
+    public Date getCreationDate() {
+        return creationDate;
     }
 }
--- src/main/java/com/example/demo/service/PostService.java
+++ src/main/java/com/example/demo/service/PostService.java
@@ -4,15 +4,16 @@ import com.example.demo.model.Post;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @Service
 public class PostService {
     public List<Post> listAllPosts() {
         List<Post> list = new ArrayList<Post>();
-        list.add(new Post("Random post 1", 7));
-        list.add(new Post("Random post 2", 9));
-        list.add(new Post("Random post 3", 11));
+        list.add(new Post("Random post 1", new Date(2024, 10, 15, 14, 00)));
+        list.add(new Post("Random post 2", new Date(2024, 10, 15, 15, 17)));
+        list.add(new Post("Random post 3", new Date(2024, 11, 12, 17, 35)));
         return list;
     }
 }
