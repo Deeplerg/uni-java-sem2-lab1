a3d93a85e53b1a357b4c54f3e0171ca094e36068
Deeplerg <deeplerg@gmail.com>
2025-02-16 21:51:01 +0500

    Add controller for creating new posts

--- src/main/java/com/example/demo/service/PostService.java
+++ src/main/java/com/example/demo/service/PostService.java
@@ -1,6 +1,7 @@
 package com.example.demo.service;
 
 import com.example.demo.model.Post;
+import com.sun.source.tree.ArrayAccessTree;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -9,11 +10,20 @@ import java.util.List;
 
 @Service
 public class PostService {
+    private List<Post> posts;
+
+    public PostService() {
+        posts = new ArrayList<>();
+        posts.add(new Post("Random post 1", new Date(2024, 10, 15, 14, 00)));
+        posts.add(new Post("Random post 2", new Date(2024, 10, 15, 15, 17)));
+        posts.add(new Post("Random post 3", new Date(2024, 11, 12, 17, 35)));
+    }
+
     public List<Post> listAllPosts() {
-        List<Post> list = new ArrayList<Post>();
-        list.add(new Post("Random post 1", new Date(2024, 10, 15, 14, 00)));
-        list.add(new Post("Random post 2", new Date(2024, 10, 15, 15, 17)));
-        list.add(new Post("Random post 3", new Date(2024, 11, 12, 17, 35)));
-        return list;
+        return posts;
+    }
+
+    public void create(String text) {
+        posts.add(new Post(text, new Date()));
     }
 }
