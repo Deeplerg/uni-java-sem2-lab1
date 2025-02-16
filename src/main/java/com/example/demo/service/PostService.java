69f2a981e76dd4af14d9dfb06d2b6be8b1c874a1
Deeplerg <deeplerg@gmail.com>
2025-02-16 23:45:32 +0500

    Add persistence

--- src/main/java/com/example/demo/service/PostService.java
+++ src/main/java/com/example/demo/service/PostService.java
@@ -1,30 +1,24 @@
 package com.example.demo.service;
 
 import com.example.demo.model.Post;
-import com.sun.source.tree.ArrayAccessTree;
+import com.example.demo.repository.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
+import java.util.stream.StreamSupport;
 
 @Service
 public class PostService {
-    private List<Post> posts;
-
-    public PostService() {
-        posts = new ArrayList<>();
-        posts.add(new Post(0L, "Random post 1", new Date(2024, 10, 15, 14, 00)));
-        posts.add(new Post(1L, "Random post 2", new Date(2024, 10, 15, 15, 17)));
-        posts.add(new Post(2L, "Random post 3", new Date(2024, 11, 12, 17, 35)));
-    }
+    @Autowired
+    private PostRepository postRepository;
 
     public List<Post> listAllPosts() {
-        return posts;
+        return StreamSupport.stream(postRepository.findAll().spliterator(), false).toList();
     }
 
     public void create(String text) {
-        Long maxId = posts.getLast().getId(); // won't work properly if we remove posts
-
-        var post = new Post(maxId + 1, text, new Date());
-        posts.add(post);
+        Post post = new Post(null, text, new Date());
+        postRepository.save(post);
     }
 }
